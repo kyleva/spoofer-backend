f2690cb68964392f0698d48dcc2e07566d795132
Branden Builds <hello@brandenbuilds.com>
2017-09-25 21:17:20 -0400

    updating with Kyle's commit

--- api/routes/spoofitem.js
+++ api/routes/spoofitem.js
@@ -14,31 +14,20 @@ router.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our api!' }); 
 });
 
-
-//post spoofer 
-
-router.post('/posts', (req, res) => {
-  let item = new SpoofItem();
-	const { title, description, image } = req.body
-  // now you have title, description, and image variables set
-  item.save(function(err) {
-    if (err)
-      res.send(err);
-
-    res.json({ message: 'Item created!' });
-  });
-
-});
-
 //show all posts
-// router.get('/posts', (req, res) => {
-// 	SpoofItem.find()
-//   .then((spoof) => {
-//   	// res = response
-//     // set status to 200 (not actually necessary but good practice)
-//     // send json object w/ spoof grabbed from db
-//     res.status(200).json(spoof)
-//   })
-// })
+router.get('/posts', (req, res) => {
+  // const mongoose = require('mongoose')
+  // const start = mongoose.connect('mongodb://localhost:27017/ogspoofer', {
+  //   useMongoClient: true
+  // })
+  // .then((db) => {
+  //   SpoofItem.find({}, function(spoofItems){
+  //     res.status(200).json(spoofItems)
+  //   })
+  // })
+  SpoofItem.find().then((spoofItems) => {
+    res.status(200).json(spoofItems)
+  })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
-    bodyParser = require('body-parser'),
-    app     = express(),
-    router = express.Router();
+bodyParser = require('body-parser'),
+app     = express(),
+router = express.Router();
 
 // configure app to use bodyParser()
 // this will let us get the data from a POS
@@ -14,7 +14,7 @@ const SpoofItem     = require('./api/models/spoofitem');
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb://localhost/spooferTest';
 mongoose.connect(mongoDB, {
-  useMongoClient: true
+useMongoClient: true
 });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -24,7 +24,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // =============================================================================
 
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+res.json({ message: 'hooray! welcome to our api!' });	
 });
 
 
@@ -44,10 +44,17 @@ app.listen(3000);
 
 //Adding Data Manually to DB
 
-// db.collection('spooferItem').insertOne( {
-//        //"spoof_name" : mongoose.Schema.Types.ObjectId,
-//        "title" : "Spoofer Title 2",
-//        "img" : "https://scontent.forf1-2.fna.fbcdn.net/v/t31.0-8/16252166_10158670472275131_2340890356782076580_o.jpg?oh=dd161e0cf4dd9742dc626316d1f363e3&oe=5A3FD9C2",
-//        "desc" : "Kogi pop-up hashtag fam prism. Try-hard letterpress single-origin coffee cliche keytar. Subway tile waistcoat meditation mumblecore cliche. ",
-//        "spoofUrl" : "http://ogspoofer/spoof1"
+// db.collection('spooferItem').insertOne({
+//   "title" : "Spoofer Title 2",
+//   "img" : "https://scontent.forf1-2.fna.fbcdn.net/v/t31.0-8/16252166_10158670472275131_2340890356782076580_o.jpg?oh=dd161e0cf4dd9742dc626316d1f363e3&oe=5A3FD9C2",
+//   "desc" : "Kogi pop-up hashtag fam prism. Try-hard letterpress single-origin coffee cliche keytar. Subway tile waistcoat meditation mumblecore cliche. ",
 // });
+
+// const spoofItem = new SpoofItem({
+// 'title': 'Spoofer Title 3',
+// 'img': 'https://scontent.forf1-2.fna.fbcdn.net/v/t31.0-8/16252166_10158670472275131_2340890356782076580_o.jpg?oh=dd161e0cf4dd9742dc626316d1f363e3&oe=5A3FD9C2',
+// 'desc': 'Kogi pop-up hashtag fam prism. Try-hard letterpress single-origin coffee cliche keytar. Subway tile waistcoat meditation mumblecore cliche.'
+// });
+// spoofItem.save((err) => {
+// console.log(err, 'saved')
+// })
