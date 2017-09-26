35acda1490e42ca4e0d1155cfd53c1a891a37013
Branden Builds <hello@brandenbuilds.com>
2017-09-26 13:23:37 -0400

    added middleware

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(function(req, res, next) {
+  // do logging
+  console.log('Something is happening.');
+  next(); // make sure we go to the next routes and don't stop here
+});
+
 router.get('/', (req, res) => {
   res.send('Home Page Info')
 });
