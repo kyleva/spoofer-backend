eccee8b5f4a0d0cd73fd034300c295350bcc9679
Branden Builds <hello@brandenbuilds.com>
2017-09-26 13:57:29 -0400

    added post route to index

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const SpoofItem = require('../models/spoofitem');
 
 router.use(function(req, res, next) {
-  // do logging
   console.log('Something is happening.');
-  next(); // make sure we go to the next routes and don't stop here
+  next(); 
 });
 
 router.get('/', (req, res) => {
@@ -12,8 +12,18 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  
-  
+    const {title, desc, img} = req.body;
+    const spoofer = new SpoofItem({ title, desc, img });
+    console.log(`Spoof Item: ${spoofer} ${spoofer.title} - ${spoofer.desc} - ${spoofer.img}` );
+
+    spoofer.save(req.body, (err, result) => {
+      if (err) return console.log(err)
+
+      console.log('saved to database');
+      
+      res.send('you\'re item has been saved');
+    })
+
 })
 
 
