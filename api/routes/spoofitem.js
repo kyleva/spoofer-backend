71c1d1b3406dea45d31732ef7dd21d377258d5e8
Branden Builds <hello@brandenbuilds.com>
2017-09-25 21:14:48 -0400

    updating syntax

--- api/routes/spoofitem.js
+++ api/routes/spoofitem.js
@@ -14,15 +14,31 @@ router.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our api!' }); 
 });
 
+
+//post spoofer 
+
+router.post('/posts', (req, res) => {
+  let item = new SpoofItem();
+	const { title, description, image } = req.body
+  // now you have title, description, and image variables set
+  item.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Item created!' });
+  });
+
+});
+
 //show all posts
-router.get('/posts', (req, res) => {
-	SpoofItem.find()
-  .then((spoof) => {
-  	// res = response
-    // set status to 200 (not actually necessary but good practice)
-    // send json object w/ spoof grabbed from db
-    res.status(200).json(spoof)
-  })
-})
+// router.get('/posts', (req, res) => {
+// 	SpoofItem.find()
+//   .then((spoof) => {
+//   	// res = response
+//     // set status to 200 (not actually necessary but good practice)
+//     // send json object w/ spoof grabbed from db
+//     res.status(200).json(spoof)
+//   })
+// })
 
 module.exports = router;
