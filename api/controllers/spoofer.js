c6a567acea7766778b4f8d7142e359f4d63f7de8
Kyle Stearns <kyle@leesa.com>
2017-10-02 19:49:42 -0400

    modifying a bit to work w/ frontend

--- api/controllers/spoofer.js
+++ api/controllers/spoofer.js
@@ -1,31 +1,31 @@
-const SpoofItem = require('../models/spoofitem');
+const SpoofItem = require("../models/spoofitem");
 const randomstring = require("randomstring");
 
-const SpoofItemController = module.exports = {
-    create: (req,res) => {
-        let name = randomstring.generate({length:8, charset: 'alphabetic'});
-        const {title, desc, img} = req.body;
-        const spoofer = new SpoofItem({ title, desc, img, name});
+const SpoofItemController = (module.exports = {
+  create: (req, res) => {
+    let name = randomstring.generate({ length: 8, charset: "alphabetic" });
+    const { title, desc, img } = req.body;
+    const spoofer = new SpoofItem({ title, desc, img, name });
 
-        spoofer.save((err) => {
-            if (err) return console.log(err)
-            console.log('saved to database');
-            res.json(spoofer);
-        })
-    },
-    detail: (req, res) => {
-        const {title, desc, img, name} = req.params;
-        SpoofItem.findOne({name: name}).then((spoofItems) => {
-            res.status(200).json({
-                title: spoofItems.title,
-                desc: spoofItems.desc,
-                img: spoofItems.img
-            })
-        })
-    },
-    list: (req, res) => {
-        SpoofItem.find().then((spoofItems) => {
-          res.status(200).json(spoofItems)
-        })
-    }
-}
+    spoofer.save((err, spoofItem) => {
+      if (err) return console.log(err);
+
+      res.json(spoofItem);
+    });
+  },
+  detail: (req, res) => {
+    const { title, desc, img, name } = req.params;
+    SpoofItem.findOne({ name: name }).then(spoofItems => {
+      res.status(200).json({
+        title: spoofItems.title,
+        desc: spoofItems.desc,
+        img: spoofItems.img
+      });
+    });
+  },
+  list: (req, res) => {
+    SpoofItem.find().then(spoofItems => {
+      res.status(200).json(spoofItems);
+    });
+  }
+});
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express'),
-bodyParser = require('body-parser'),
-cors = require('cors'),
-app     = express(),
-router = express.Router();
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  cors = require("cors"),
+  app = express(),
+  router = express.Router();
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 // configure app to use bodyParser()
@@ -14,37 +14,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // CONNECT TO DATABASE -------------------------------
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
-useMongoClient: true
+  useMongoClient: true
 });
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // ROUTES FOR OUR API
 // =============================================================================
 
 // BRING IN OUR ROUTES -------------------------------
-const index = require('./api/routes/index');
-const spoofItemRoute = require('./api/routes/spoofitem');
-
+const index = require("./api/routes/index");
+const spoofItemRoute = require("./api/routes/spoofitem");
 
 // REGISTER OUR ROUTES -------------------------------
 
-//set up cross orgin resource sharing 
+//set up cross orgin resource sharing
 
 const corsOptions = {
-  origin: process.env.ORGIN_URL,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-}
+  origin: process.env.ORIGIN_URL,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-app.use('/api', cors(corsOptions), spoofItemRoute);
-app.use('/', index);
+app.use("/api", cors(corsOptions), spoofItemRoute);
+app.use("/", index);
 
 // VIEWS
 // =============================================================================
-app.set('views', __dirname + '/api/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/api/views");
+app.set("view engine", "ejs");
 
 app.listen(3000);
