2fcb42f85637eb14fb8a8b64e3a93e6f0dbc9ce4
Branden Builds <hello@brandenbuilds.com>
2017-10-11 11:10:02 -0400

    Implmented rursive function on controller to regenarte name property until there is no duplicate

--- api/controllers/spoofer.js
+++ api/controllers/spoofer.js
@@ -5,11 +5,17 @@ const SpoofItemController = (module.exports = {
   create: (req, res) => {
     let name = randomstring.generate({ length: 8, charset: "alphabetic" });
     const { title, desc, img } = req.body;
+    generateName = (callback) => {
+      let name = randomstring.generate({ length: 8, charset: "alphabetic" });
+      SpoofItem.find({ name }, (err, results) => {
+        if (err) return callback(err, null);
+        if (results.length === 0) return callback(null, name);
+        return generateName(callback);
+      });
+    }
     const spoofer = new SpoofItem({ title, desc, img, name });
-
     spoofer.save((err, spoofItem) => {
       if (err) return console.log(err);
-
       res.json(spoofItem);
     });
   },
--- api/models/spoofitem.js
+++ api/models/spoofitem.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const SpoofItemSchema = mongoose.Schema({
-	name: {type: String},
+	name: {type: String, unique: true},
 	title: {type: String, required: true},
-    desc: {type: String, reuired: true, max: 300},
-    img: {type: String, reuired: true}
-    //places_id: {type: Date, default: Date.now}
+    desc: {type: String, required: true, max: 300},
+    img: {type: String, required: true}
 });
 
 module.exports = mongoose.model('SpoofItem', SpoofItemSchema);
