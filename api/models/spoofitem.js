6bbcec1a0af5bf692698be687935450be9bd7a90
Branden Builds <hello@brandenbuilds.com>
2017-10-11 22:30:27 -0400

    added more validation to model

--- api/models/spoofitem.js
+++ api/models/spoofitem.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const SpoofItemSchema = mongoose.Schema({
-	name: {type: String, unique: true},
-	title: {type: String, required: true},
-    desc: {type: String, required: true, max: 300},
+	name: {type: String, unique: true, },
+	title: {type: String, required: true, minlength: 1, trim: true},
+    desc: {type: String, required: true, max: 300, minlength: 1, trim: true},
     img: {type: String, required: true}
 });
 
